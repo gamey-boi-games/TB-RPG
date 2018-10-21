f8c82fa041295613abfa7272eee837d47d6ef72d
Delenko <34238377+delenko@users.noreply.github.com>
2018-10-20 17:57:28 -0700

    implemented get/set
    Changed item to reflect new layout

--- Item.cpp
+++ Item.cpp
@@ -1,33 +1,72 @@
-#include <iostream>
-#include <string>
 #include "Item.hpp"
 
+#include <iostream>
+#include <string>
+
 Item::Item() {
-
-};
-
-//Establish weapons
-struct weapons {
-	std::string name;
-	int quantity;
-	int damage;
-	//bonus effects
-};
-//establish armors
-struct armors {
-	std::string name;
-	int quantity;
-	int defense;
-};
-//establish potions
-struct potions {
-	std::string name;
-	int quantity;
-	int health;
-};
+	health = 0;
+	defense = 0;
+	attack = 0;
+	mana = 0;
+	special = 0;
+	ID = 0;
+	name = "";
+	}
 
 
+	Item::Item(unsigned long bonusHP, unsigned long bonusDef, unsigned long bonusAT, unsigned long bonusMana, unsigned long bonusSP,int identity, std::string nameOfItem) {
+		health = bonusHP;
+		defense = bonusDef;
+		attack = bonusAT;
+		mana = bonusMana;
+		special = bonusSP;
+		ID = identity;
+		name = nameOfItem;
+	};
+	
+	void Item::setHealth(unsigned long bonusHP) {
+		health = bonusHP;
+	}
 
-void Item::returnItemEffect(Item& item) {
+	void Item::setAttack(unsigned long bonusAT) {
+		attack = bonusAT;
+	}
 
-}
+	void Item::setDefense(unsigned long bonusDef) {
+		defense = bonusDef;
+	}
+
+	void Item::setMana(unsigned long bonusMana) {
+		mana = bonusMana;
+	}
+
+	void Item::setSpecial(unsigned long bonusSP) {
+		special = bonusSP;
+	}
+
+	unsigned long Item::getAttack() {
+		return attack;
+	}
+
+	unsigned long Item::getHealth() {
+		return health;
+	}
+
+	unsigned long Item::getDefense() {
+		return defense;
+	}
+
+	unsigned long Item::getMana() {
+		return mana;
+	}
+
+	unsigned long Item::getSpecial() {
+		return special;
+	}
+
+
+
+	void Item::returnItemEffect(Item& item) {
+
+	}
+
