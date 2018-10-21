bc70a578c013d4814670cfab12cdfab47137af4d
Delenko <34238377+delenko@users.noreply.github.com>
2018-10-20 17:56:43 -0700

    Get/Set
    Added getters and setters to items. will more than likely have to make unique functions for each item to be created in their respective categories

--- Item.hpp
+++ Item.hpp
@@ -1,9 +1,35 @@
 #pragma once
+#include <string>
 
 class Item {
 public:
+	Item(unsigned long bonusHP, unsigned long bonusDef, unsigned long bonusAT, unsigned long bonusMana, unsigned long bonusSP, int identity,std::string nameOfItem);
 	Item();
+	//setters to make the variables set.
+	void setHealth(unsigned long bonusHP);
+	void setDefense(unsigned long bonusDEF);
+	void setAttack(unsigned long bonusAT);
+	void setMana(unsigned long bonusMana);
+	void setSpecial(unsigned long bonusSP);
+
+	//getters to grab the values
+	unsigned long getHealth();
+	unsigned long getDefense();
+	unsigned long getAttack();
+	unsigned long getMana();
+	unsigned long getSpecial();
+
 
 	void returnItemEffect(Item& item);
+
+private:
+	unsigned long health;
+	unsigned long defense;
+	unsigned long attack;
+	unsigned long mana;
+	unsigned long special;
+	int ID;
+	std::string name;
+
 
 };
